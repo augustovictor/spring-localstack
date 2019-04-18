469461324f4f6c2f0db54e8ddfbecd13531a99b2
Victor Costa <victor.costa@creditas.com.br>
2019-04-18 17:15:45 -0300

    [FEAT] Add BaseIntegrationTest that loads LocalStackContainer for tests

---
+++ src/test/kotlin/com/augustovictor/localstack/BaseIntegrationTest.kt
@@ -0,0 +1,28 @@
+package com.augustovictor.localstack
+
+import org.junit.Rule
+import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import org.testcontainers.containers.localstack.LocalStackContainer
+import org.testcontainers.containers.localstack.LocalStackContainer.Service.S3
+
+@ContextConfiguration(initializers = [BaseIntegrationTest.Initializer::class])
+abstract class BaseIntegrationTest {
+
+    companion object {
+        @get:Rule
+        val localStackContainer = LocalStackContainer().withServices(S3).apply { start() }
+    }
+
+
+    object Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(applicationContext: ConfigurableApplicationContext) {
+            val values = TestPropertyValues.of("aws.s3.uri=${localStackContainer.getEndpointConfiguration(S3).serviceEndpoint}")
+
+            values.applyTo(applicationContext)
+        }
+
+    }
+}
---
+++ src/test/kotlin/com/augustovictor/localstack/fileManager/FileManager.kt
@@ -0,0 +1,18 @@
+package com.augustovictor.localstack.fileManager
+
+import com.augustovictor.localstack.BaseIntegrationTest
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FileManagerTest : BaseIntegrationTest() {
+
+    @Test
+    fun `asd ads`() {
+        assertTrue(true)
+    }
+}
