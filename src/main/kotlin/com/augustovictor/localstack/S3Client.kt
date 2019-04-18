76708e817449226afa9ab0f7ac2adfb8b025814b
Victor Costa <victor.costa@creditas.com.br>
2019-04-18 16:18:42 -0300

    [REFACTOR] Remove dead code

--- src/main/kotlin/com/augustovictor/localstack/S3Client.kt
+++ src/main/kotlin/com/augustovictor/localstack/S3Client.kt
@@ -3,11 +3,9 @@ package com.augustovictor.localstack
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.PutObjectRequest
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.lang.Exception
 
 @Configuration
 class S3Config(
@@ -20,24 +18,5 @@ class S3Config(
         val endpointConfiguration = AwsClientBuilder.EndpointConfiguration(s3Uri, "us-east-1")
         if (s3Uri != "") s3ClientBuilder.withEndpointConfiguration(endpointConfiguration)
         return s3ClientBuilder.build()
-    }
-
-    @Bean
-    fun applicationRunnerS3(amazonS3: AmazonS3): Boolean {
-        try {
-            amazonS3.createBucket("my-custom-bucket")
-        } catch(e: Exception) {
-            println(e)
-        }
-
-        println("Buckets:")
-        amazonS3.listBuckets().forEach(::println)
-
-//        val bucket = "my-custom-bucket"
-//        amazonS3.createBucket(bucket)
-//
-//        amazonS3.putObject(PutObjectRequest(bucket, "object", "My custom data".byteInputStream(), null))
-
-        return true
     }
 }
