66fa0627aca8908baede92ec73c2827be6d19680
augustovictor <victoraweb@gmail.com>
2019-04-25 21:21:02 -0300

    [FEAT] Add endpoint to remove bucket

--- src/main/kotlin/com/augustovictor/localstack/FileManagerController.kt
+++ src/main/kotlin/com/augustovictor/localstack/FileManagerController.kt
@@ -4,10 +4,9 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.CannedAccessControlList
 import com.amazonaws.services.s3.model.ObjectListing
 import com.amazonaws.services.s3.model.PutObjectRequest
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.NO_CONTENT
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
@@ -44,5 +43,11 @@ class FileManagerController(private val amazonS3: AmazonS3) {
             @RequestParam("object-key") objectKey: String
     ): String {
         return amazonS3.getUrl(bucketName, objectKey).toExternalForm()
+    }
+
+    @DeleteMapping
+    @ResponseStatus(NO_CONTENT)
+    fun deleteBucket(@RequestParam("bucket-name") bucketName: String) {
+        amazonS3.deleteBucket(bucketName)
     }
 }
